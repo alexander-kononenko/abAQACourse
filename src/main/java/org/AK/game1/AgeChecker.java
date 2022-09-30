d69b102a2b3110c127100a31a1f45778a9f358cb
kononenko192@gmail.com <alexander-kononenko>
2022-09-30 21:47:49 +0300

    + negative test in new branch

--- src/main/java/org/AK/game1/AgeChecker.java
+++ src/main/java/org/AK/game1/AgeChecker.java
@@ -1,7 +1,7 @@
 package org.AK.game1;
 
 public class AgeChecker {
-    private final int MIN_AGE = 18;
+    private final int MIN_AGE = 21;
 
     public boolean CanPlayGameAge (int age) {
         return age >= MIN_AGE;
--- src/test/java/org/AK/game1/AgeCheckerTest1.java
+++ src/test/java/org/AK/game1/AgeCheckerTest1.java
@@ -5,8 +5,14 @@ import org.testng.annotations.Test;
 
 public class AgeCheckerTest1 {
     @Test
-    public void testAgedUsedCanPlay (){
+    public void testAgedUserCanPlay (){
         AgeChecker ageChecker = new AgeChecker();
-        Assert.assertTrue(ageChecker.CanPlayGameAge(21), "User can Play");
+        Assert.assertTrue(ageChecker.CanPlayGameAge(18), "User can Play and is older than 18");
+    }
+    @Test
+    public void testThatTooYongUsersCanNotPlay () {
+        AgeChecker ageChecker = new AgeChecker();
+        Assert.assertFalse(ageChecker.CanPlayGameAge(11), "User is too young");
     }
 }
+
